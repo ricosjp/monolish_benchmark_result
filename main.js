6b79b0957706b8201bd58bdb6ec0a708fb8b77d8
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-11-09 06:12:34 +0900

    select prec

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const search_axpy_data = (func, precision) => {
-    const arch='cpu'
+const search_axpy_data = (func, arch, precision) => {
     const version = '74226f9cf5033de9ba8be64a1b37cbfdf1a33733'
     let x = [];
     let y = [];
@@ -21,34 +20,70 @@ const search_axpy_data = (func, precision) => {
 };
 
 const create_func_checkbox = () => {
-    let p = document.createElement('p');
-    p.innerHTML = '<form name = "select_func_form">';
+
+    // checkbox func
+    let p1 = document.createElement('p');
+    p1.innerHTML = '<form name = "select_func_form">';
     for (const a in func_list) {
         p.innerHTML += '<input type="checkbox" name="checked_func" value=' + func_list[a] + '>'+func_list[a]+'</input>';
     }
-    p.innerHTML += '</select_func_form>';
-    p.innerHTML += '<br>';
-    p.innerHTML += '<br>';
-    p.innerHTML += '<input type="button" value="Plot!" onclick="clickBtn()"/>'
-    document.body.appendChild(p);
+    p1.innerHTML += '</select_func_form>';
+    document.body.appendChild(p1);
+
+    // checkbox arch
+    let p2 = document.createElement('p');
+    p2.innerHTML = '<form name = "select_arch_form">';
+    for (const a in arch_list) {
+        p2.innerHTML += '<input type="checkbox" name="checked_arch" value=' + arch_list[a] + '>'+arch_list[a]+'</input>';
+    }
+    p2.innerHTML += '</select_arch_form>';
+    document.body.appendChild(p2);
+
+    // checkbox func
+    let p3 = document.createElement('p');
+    p3.innerHTML = '<form name = "select_prec_form">';
+    for (const a in prec_list) {
+        p3.innerHTML += '<input type="checkbox" name="checked_prec" value=' + prec_list[a] + '>'+prec_list[a]+'</input>';
+    }
+    p3.innerHTML += '</select_prec_form>';
+    p3.innerHTML += '<br>';
+    p3.innerHTML += '<br>';
+    p3.innerHTML += '<input type="button" value="Plot!" onclick="clickBtn()"/>'
+    document.body.appendChild(p3);
 };
 
 const clickBtn = () => {
 	const funcs = [];
 	const checked_func = document.getElementsByName("checked_func");
-
     for (let i=0; i<checked_func.length; i++){
         if (checked_func[i].checked){
             funcs.push(checked_func[i].value);
         }
     }
-    plot_result(funcs);
+
+	const archs = [];
+	const checked_arch = document.getElementsByName("checked_arch");
+    for (let i=0; i<checked_arch.length; i++){
+        if (checked_arch[i].checked){
+            archs.push(checked_arch[i].value);
+        }
+    }
+
+	const precs = [];
+	const checked_prec = document.getElementsByName("checked_prec");
+    for (let i=0; i<checked_prec.length; i++){
+        if (checked_prec[i].checked){
+            precs.push(checked_prec[i].value);
+        }
+    }
+
+    plot_result(funcs, archs, precs);
 
 };
 
-const plot_result = (funcs) =>{
+const plot_result = (funcs, archs, precs) =>{
 
-    plot_data = [];
+    let plot_data = [];
 
     const layout = {
         yaxis: {
@@ -63,25 +98,21 @@ const plot_result = (funcs) =>{
 
     // create plot data
     for(let i=0; i< funcs.length; i++){
-        let plot_data_double = search_axpy_data(funcs[i], 'double');
-        let plot_data_float = search_axpy_data(funcs[i], 'float');
+        for(let j=0; j< archs.length; j++){
+            for(let k=0; k< precs.length; k++){
 
-        const float_data = {
-            x: plot_data_double[0],
-            y: plot_data_double[1],
-            name: funcs[i] + '_double',
-            type: 'scatter'
-        };
+                const plot_element_data = search_axpy_data(funcs[i], archs[j], precs[k]);
 
-        const double_data = {
-            x: plot_data_float[0],
-            y: plot_data_float[1],
-            name: funcs[i] + '_float',
-            type: 'scatter'
-        };
+                const plot_element = {
+                    x: plot_element_data[0],
+                    y: plot_element_data[1],
+                    name: funcs[i] + '_' + precs[k] + '_' + archs[j],
+                    type: 'scatter'
+                };
 
-        plot_data.push(float_data);
-        plot_data.push(double_data);
+                plot_data.push(plot_element);
+            }
+        }
     }
 
     Plotly.newPlot('myDiv', plot_data, layout, config);
