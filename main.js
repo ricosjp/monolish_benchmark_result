73ac2c8889338c97eb81a464979b31dc09a5cc2f
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-11-09 06:32:00 +0900

    x axis log scale

--- main.js
+++ main.js
@@ -94,7 +94,16 @@ const plot_result = (funcs, archs, precs) =>{
             title: 'Size',
             type: 'log',
             autorange: 'true'
-        }
+        },
+        width: 800,
+        height: 800,
+        margin: {
+            l: 50,
+            r: 50,
+            b: 100,
+            t: 100,
+            pad: 4
+        },
     };
 
     const config = {};
